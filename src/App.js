eb57f3022326893b443088348a9a946cf2eb483c
kairos67 <kairosys@gmail.com>
2022-05-25 19:46:30 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState([])
+  const addItem=()=>{
+    setTodoList([...todoList,inputValue])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <input
+        value={inputValue}
+        type="text"
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <button onClick={addItem}>추가</button>
+    </main>
   );
 }
 
